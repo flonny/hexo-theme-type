8c9996ddc9d3dfb38226bfa73c5d73472c5e0cdb
fanyuedong <632296950@qq.com>
2019-09-26 15:57:49 +0800

    首页

--- source/js/app.js
+++ source/js/app.js
@@ -1,23 +1,32 @@
 $(document).ready(function() {
-
-  'use strict';
+  "use strict";
 
   // =================
   // Responsive videos
   // =================
 
-  $('.o-wrapper').fitVids({
-    'customSelector': ['iframe[src*="ted.com"]']
-  });
+  // $(".o-wrapper").fitVids({
+  //   customSelector: ['iframe[src*="ted.com"]']
+  // });
 
   // =================
   // Off Canvas menu
   // =================
-
-  $('.js-off-canvas-toggle').click(function(e) {
+$(".c-off-canvas-content").click(function(e) {
+  e.stopPropagation();
+})
+  $(".js-off-canvas-toggle").click(function(e) {
     e.preventDefault();
-    $('.js-off-canvas-toggle').toggleClass('is-active');
-    $('.js-off-canvas-container').toggleClass('is-active');
+    e.stopPropagation();
+    $(".js-off-canvas-toggle").toggleClass("is-active");
+    $(".js-off-canvas-container").toggleClass("is-active");
+    $(".js-off-canvas-container.is-active").click(function(e) {
+      if ($(this).hasClass("is-active")) {
+        $(".js-off-canvas-toggle").toggleClass("is-active");
+        $(".js-off-canvas-container").toggleClass("is-active");
+      }else{
+        $(this).unbind('click')
+      }
+    });
   });
-
 });
